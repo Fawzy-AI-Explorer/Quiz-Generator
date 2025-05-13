27dec638d4b8d600a4e8960126a3eb59bb831e48
Adham 3llam <102502789+Ad7amstein@users.noreply.github.com>
2025-05-13 22:58:35 +0300

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -2,4 +2,4 @@
 
 # File Paths
 DATA_PATH   = r"E:\DATA SCIENCE\projects\Agents\01-Quiz generator\data\[7] RNN-GRU-LSTM.pdf"
-OUTPUT_PATH = r"E:\DATA SCIENCE\projects\Agents\01-Quiz generator\output"
+OUTPUT_PATH = r"/mount/src/quiz-generator/output/"
