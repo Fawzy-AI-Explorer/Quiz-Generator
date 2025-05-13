0c1077b7cdaa13ae74213cb148e6a6a750220bad
Ad7amstein <adham32003200@gmail.com>
2025-05-13 17:13:49 +0300

    add my paths

--- src/config/config.py
+++ src/config/config.py
@@ -1,5 +1,5 @@
 # This file contains the configuration for the LLM provider.
 
 # File Paths
-DATA_PATH   = r"E:\DATA SCIENCE\projects\Agents\01-Quiz generator\data\[7] RNN-GRU-LSTM.pdf"
-OUTPUT_PATH = r"E:\DATA SCIENCE\projects\Agents\01-Quiz generator\output"
+DATA_PATH   = r"E:\Data Science\Projects\crewai\quiz_generator\Explanation of NLP Embedding Methods.pdf"
+OUTPUT_PATH = r"E:\Data Science\Projects\crewai\Quiz-Generator\output"
