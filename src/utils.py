551a2daeeb3f0fa83d7ca65dccad419485a52550
Ad7amstein <adham32003200@gmail.com>
2025-05-13 12:39:47 +0300

    create output dir

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
 from pypdf import PdfReader
 import json_repair
+import os
+import shutil
+
 
 def process_pdf(file_path: str) -> str:
     """Extract text content from PDF files"""
@@ -12,17 +15,19 @@ def process_pdf(file_path: str) -> str:
     except Exception as e:
         return f"Error reading PDF: {str(e)}"
 
-# try:
-#     t = process_pdf(r"E:\DATA SCIENCE\projects\Agents\01-Quiz generator\data\[7] RNN-GRU-LSTM.pdf")
-#     print(t)
-# except Exception as e:
-#     print(f"Error: {str(e)}")
-
 def parse_json(text):
     """Parse JSON content and repair if necessary"""
     try:
         return json_repair.loads(text)
     except:
         return None
-    
+
+def create_output_dir(OUTPUT_PATH):
+    if os.path.exists(OUTPUT_PATH):
+        shutil.rmtree(OUTPUT_PATH)
+
+    os.mkdir(OUTPUT_PATH)
+    print("Output Path Created Successfully!")
+
+
 # to run this : python crew_ai/utils.py
